e9967c21965a0215740f83ea79fad312f5b008fe
Max Van Aken <mtvanaken@gmail.com>
2019-07-03 14:04:33 +0100

    First Commit - README

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,12 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
+// var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
+
+  
 
 });
